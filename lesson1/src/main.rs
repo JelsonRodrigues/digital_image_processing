399262ceca14ec3d638015751ddc0c317a2270f9
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2024-12-03 17:51:10 -0300

    chore: invert one image

--- lesson1/src/main.rs
+++ lesson1/src/main.rs
@@ -1,13 +1,31 @@
-use clap::Parser;
+use std::u8;
 
+use clap::Parser;
+use image::{DynamicImage, GenericImage, ImageBuffer, ImageDecoder, ImageReader};
+use rayon::prelude::*;
 #[derive(Debug, Parser)]
 struct Args {
-    #[clap(short, long)]
-    input_image: String,
+    #[clap(long)]
+    img: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Input image: {}", args.input_image);
+    println!("Input image1: {}", args.img);
+
+    let mut img = ImageReader::open(args.img).unwrap().decode().unwrap();
+
+    img.as_mut_rgba8()
+        .unwrap()
+        .pixels_mut()
+        .into_iter()
+        .for_each(|pixel| {
+            pixel[0] = u8::MAX - pixel[0];
+            pixel[1] = u8::MAX - pixel[1];
+            pixel[2] = u8::MAX - pixel[2];
+        });
+
+    // save inverted image
+    img.save("inverted.png").unwrap();
 }
